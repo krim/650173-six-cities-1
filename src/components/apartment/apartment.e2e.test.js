615bc77292b07a0891bdd3d8231e8c1786c2ed70
Pavel Rodionov <pasha.rod@mail.ru>
2019-05-09 13:01:51 +0300

    Add a component for render list of apartments. Add apartments mock

--- src/components/apartment/apartment.e2e.test.js
+++ src/components/apartment/apartment.e2e.test.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Apartment from './apartment';
 
-Enzyme.configure({adapter: new Adapter()});
+import Apartment from './apartment';
+import apartment from './../../__fixtures__/apartment';
+
+configure({adapter: new Adapter()});
 
 describe(`Apartment`, () => {
-  it(`App correctly renders after relaunch`, () => {
+  it(`correctly renders after relaunch and handles events`, () => {
     const apartmentNameClickHandler = jest.fn();
+    const apartmentMouserEnterHandler = jest.fn();
+    const apartmentMouserOverHandler = jest.fn();
     const app = shallow(<Apartment
-      apartment={{name: `apartment name`}}
+      apartment={apartment}
       apartmentNameClick={apartmentNameClickHandler}
+      apartmentMouserEnter={apartmentMouserEnterHandler}
+      apartmentMouserOver={apartmentMouserOverHandler}
     />);
-    const apartmentName = app.find(`.place-card__name a`);
 
+    const apartmentName = app.find(`.place-card__name a`);
     apartmentName.simulate(`click`, {preventDefault() {}});
     expect(apartmentNameClickHandler).toHaveBeenCalledTimes(1);
+
+    const apartmentCard = app.find(`.cities__place-card`);
+
+    apartmentCard.simulate(`mouseover`);
+    expect(apartmentMouserEnterHandler).toHaveBeenCalledTimes(1);
+
+    apartmentCard.simulate(`mouseout`);
+    expect(apartmentMouserOverHandler).toHaveBeenCalledTimes(1);
   });
 });
