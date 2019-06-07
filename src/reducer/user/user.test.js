e13a789ed2f4cba92f2a7485a6fcae955b9badc2
Pavel Rodionov <pasha.rod@mail.ru>
2019-06-07 19:07:16 +0300

    add user tests

--- src/reducer/user/user.test.js
+++ src/reducer/user/user.test.js
@@ -2,18 +2,40 @@ import {
   ActionType,
   Operation
 } from './user';
+import MockAdapter from 'axios-mock-adapter';
+import camelcaseKeys from 'camelcase-keys';
+import {createAPI} from '../../api';
+
+const userResponse = {
+  "id": 1,
+  "name": `user name`,
+  "email": `example@email.comu`,
+  "avatar_url": `user_url`,
+  "is_pro": true
+};
 
 describe(`Operation`, () => {
   describe(`authorize`, () => {
-    xit(`authorizes a user`, () => {
+    it(`authorizes a user`, () => {
+      const api = createAPI({push: jest.fn()});
+      const apiMock = new MockAdapter(api);
       const dispatch = jest.fn();
-      Operation.authorize()(dispatch);
+      const history = {push: jest.fn()};
+      const userData = {email: `example@email.com`, password: `password`};
+      const authorize = Operation.authorize(userData, {history});
 
-      expect(dispatch).toHaveBeenCalledTimes(1);
-      expect(dispatch).toHaveBeenNthCalledWith(1, {
-        type: ActionType.AUTHORIZATION,
-        payload: true
-      });
+      apiMock
+        .onPost(`/login`, userData)
+        .reply(200, JSON.stringify(userResponse));
+
+      authorize(dispatch, jest.fn(), api)
+        .then(() => {
+          expect(dispatch).toHaveBeenCalledTimes(1);
+          expect(dispatch).toHaveBeenNthCalledWith(1, {
+            type: ActionType.AUTHORIZATION,
+            payload: camelcaseKeys(userResponse),
+          });
+        });
     });
   });
 });
